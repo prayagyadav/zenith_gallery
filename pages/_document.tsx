7d512f610dc6c6adb2603863e833a6bacc4c37ac
prayagyadav <prayagyadav206@gmail.com>
2024-04-04 19:23:37 +0530

    Automated Commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,20 +8,14 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Zenith Astro Club"
           />
-          <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
+          <meta property="og:site_name" content="https://zenith-gallery.vercel.app" />
           <meta
             property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Zenith Astro Club"
           />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
-          <meta
-            name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
-          />
+          <meta property="og:title" content="Zenith Astro Club" />
         </Head>
         <body className="bg-black antialiased">
           <Main />
